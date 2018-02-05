bad7beb75f512302d047bdcf4506c35fead65777
ntrsBIG <bnn1911@gmail.com>
2018-02-05 20:21:03 +0400

    Added setting keystroke dialog.

--- trayicon.cpp
+++ trayicon.cpp
@@ -2,11 +2,12 @@
 #include "trayicon.h"
 
 TrayIcon::TrayIcon()
-    : translateKeySequence("Ctrl+C+X")
 {
     setKeySequence = new QAction(tr("&Set the shortcut"));
     quit = new QAction(tr("&Quit"),this);
-    connect(setKeySequence, &QAction::triggered, new KeySequenceDialog(), &QDialog::open);
+    setDialog = new KeySequenceDialog(QKeySequence(tr("Alt+1")));
+
+    connect(setKeySequence, &QAction::triggered, setDialog, &QDialog::open);
     connect(quit, &QAction::triggered, qApp, &QGuiApplication::quit);
 
     trayIconMenu = new QMenu();
@@ -18,7 +19,63 @@ TrayIcon::TrayIcon()
     setIcon(QIcon(":/icon.svg"));
 }
 
-KeySequenceDialog::KeySequenceDialog() {
-    setWindowTitle("Set the shorcut");
+KeySequenceDialog::KeySequenceDialog(QKeySequence keySequence)
+    : translateKeySequence(keySequence)
+{
+    labelBeforeField = new QLabel(tr("Current shortcut: "), this);
+
+    keySequenceField = new QLineEdit(translateKeySequence.toString(), this);
+    keySequenceField->setReadOnly(true);
+
+    setButton = new QPushButton(tr("Enter new shortcut"),this);
+
+    labelAfterButton = new QLabel(tr("Press any combination of keys (Ctrl+Shift+D, Alt+5, ...)\n"
+                          "Several combinations may not work on your operation system."));
+    labelAfterButton->setVisible(false);
+
+    connect(setButton,&QPushButton::clicked,this,[&](){ isClicked = true; labelAfterButton->setVisible(true); });
+
+    QGridLayout* layout = new QGridLayout(this);
+    layout->addWidget(labelBeforeField,0,0);
+    layout->addWidget(keySequenceField,0,1);
+    layout->addWidget(setButton,1,1);
+    layout->addWidget(labelAfterButton,2,0,1,2);
+    layout->setSizeConstraint(QLayout::SetFixedSize);
+    setLayout(layout);
+
+    setWindowTitle("Set the translate shortcut");
+}
+
+void KeySequenceDialog::keyPressEvent(QKeyEvent* keyEvent) {
+    if (isClicked) {
+        int keyInt = keyEvent->key();
+
+        if (keyInt == Qt::Key_unknown) return;
+
+        // Only single click of modifier
+        if (keyInt == Qt::Key_Control || keyInt == Qt::Key_Shift   ||
+            keyInt == Qt::Key_Alt     || keyInt == Qt::Key_Meta)
+            return;
+
+        Qt::KeyboardModifiers modifiers = keyEvent->modifiers();
+
+        if(modifiers & Qt::ShiftModifier)
+            keyInt += Qt::SHIFT;
+        if(modifiers & Qt::ControlModifier)
+            keyInt += Qt::CTRL;
+        if(modifiers & Qt::AltModifier)
+            keyInt += Qt::ALT;
+        if(modifiers & Qt::MetaModifier)
+            keyInt += Qt::META;
+
+        translateKeySequence = QKeySequence(keyInt);
+        keySequenceField->setText(translateKeySequence.toString());
+
+        isClicked = false;
+
+        labelAfterButton->setVisible(false);
+    }
 
 }
+
+
--- trayicon.h
+++ trayicon.h
@@ -1,12 +1,17 @@
 #ifndef TRAYICON_H
 #define TRAYICON_H
 
-#include <QSystemTrayIcon>
-#include <QDialog>
 #include <QAction>
+#include <QDialog>
 #include <QSystemTrayIcon>
 #include <QMenu>
 #include <QKeySequence>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QGridLayout>
+
+class KeySequenceDialog;
 
 class TrayIcon : public QSystemTrayIcon
 {
@@ -19,16 +24,30 @@ private:
     QAction* setKeySequence;
     QAction* quit;
 
-    QKeySequence translateKeySequence;
-
     QMenu* trayIconMenu;
+
+    KeySequenceDialog* setDialog;
 };
 
 class KeySequenceDialog : public QDialog {
     Q_OBJECT
 
 public:
-    KeySequenceDialog();
+    explicit KeySequenceDialog(QKeySequence keySequence);
+
+    QKeySequence getTranslateKeySequence() const { return translateKeySequence; }
+
+protected:
+    void keyPressEvent(QKeyEvent*) override;
+
+private:
+    QLabel* labelBeforeField;
+    QLineEdit* keySequenceField;
+    QLabel* labelAfterButton;
+    QPushButton* setButton;
+    QKeySequence translateKeySequence;
+
+    bool isClicked;
 };
 
 #endif // TRAYICON_H
